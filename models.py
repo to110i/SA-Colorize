147592a90142d21176c4b3a391e0d096967edd2b
to110i <beealive1213@gmail.com>
2021-05-17 21:06:50 +0900

    commit from lab

---
+++ models.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.conv import Conv2d
+from spectral import SpectralNorm
+
+
+class Self_Attn(nn.Module):
+    """Self attention Layer """
+
+    def __init__(self, in_dim, activation):
+        super(Self_Attn, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim/8, kernel_size=1)
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps (B x C x W x H)
+            returns :
+                out : self attention value + input feature
+                attention : B x N x N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+        proj_query = self.query_conv(x).view(
+            m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width*height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
+
+
+class Generator(nn.Module):
+    """Generator"""
+
+    def __init__(self, batch_size, image_size=64, conv_dim=64):
+        super(Generator, self).__init__()
+        self.imsize = image_size
+        layer1 = []
+        layer2 = []
+        layer3 = []
+        last = []
+
+
+class Discriminator(nn.Module):
+    def __init__(self, batch_size=64, image_size=64, conv_dim=64):
+        super(Discriminator, self).__init__()
+        self.imsize = image_size
+
+        layer1 = []
+        layer2 = []
+        layer3 = []
+        last = []
+
+        layer1.append(SpectralNorm(nn.Conv2d(3, conv_dim, 4, 2, 1)))
+        layer1.append(nn.LeakyReLU(0.1))
+
+        curr_dim = conv_dim
+
+        layer2.append(SpectralNorm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+        layer2.append(nn.LeakyReLU(0.1))
+
+        curr_dim = curr_dim * 2
+
+        layer3.append(SpectralNorm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+        layer3.append(nn.LeakyReLU(0.1))
+
+        curr_dim = curr_dim * 2
+
+        if self.imsize == 64:
+            layer4 = []
+            layer4.append(SpectralNorm(
+                nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+            self.l4 = nn.Sequential(*layer4)
+            curr_dim = curr_dim * 2
+
+        self.l1 = nn.Sequential(*layer1)
+        self.l2 = nn.Sequential(*layer2)
+        self.l3 = nn.Sequential(*layer3)
+
+        last.append(nn.Conv2d(curr_dim, 1, 4))
+        self.last = nn.Sequential(*last)
+
+        self.attn1 = Self_Attn(256, 'relu')
+        self.attn2 = Self_Attn(512, 'relu')
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.l2(out)
+        out = self.l3(out)
+        out, p1 = self.attn1(out)
+        out = self.l4(out)
+        out, p2 = self.attn2(out)
+        out = self.last(out)
+
+        return out.squeeze(), p1, p2
--- train.py
+++ train.py
@@ -1,4 +1,11 @@
 import torch
+import kornia
+import time
+import os
+import datetime
+from torchvision.utils import save_image
+
+from models import Generator, Discriminator
 
 
 class Trainer(object):
@@ -16,23 +23,26 @@ class Trainer(object):
 
     def train(self) -> None:
         data_iter = iter(self.dataloader)
+        start_time = time.time()
 
-        for epoch in range(epochs):
+        for step in range(self.total_step):
             self.D.train()
             self.G.train()
 
             try:
-                real_L, real_ab = next(data_iter)
+                real_img = next(data_iter)
 
             except:
                 data_iter = iter(self.dataloader)
-                real_L, real_ab = next(data_iter)
-            # real_images = tensor2var(real_images) 必要？？
-
+                real_img = next(data_iter)
+            real_Lab = kornia.color.rgb_to_lab(real_img)
+            real_Lab = real_Lab.cuda()
+            # B x C x H x W
             # dr1, dr2, df1, df2, gf1, gf2 are attention scores
-            d_out_real, dr1, dr2 = self.D(real_ab)
+            d_out_real, dr1, dr2 = self.D(real_Lab)
             d_loss_real = self.calc_loss(d_out_real, True)
-
+            real_L = real_Lab[:, 0, :, :]
+            real_ab = real_Lab[:, 1:3, :, :]
             fake_ab, gf1, gf2 = self.G(real_L)
             d_out_fake, df1, df2 = self.D(fake_ab)
             d_loss_fake = self.calc_loss(d_out_fake, False)
@@ -41,3 +51,69 @@ class Trainer(object):
             self.reset_grad()
             d_loss.backward()
             self.d_optimizer.step()
+
+            if self.adv_loss == 'wgan-gp':
+                alpha = torch.rand(real_ab.size(
+                    0), 1, 1, 1).cuda().expand_as(real_Lab)
+                interpolated = torch.autograd.Variable(
+                    alpha * real_Lab.data + (1 - alpha) * fake_Lab.data, requires_grad=True)
+                out, _, _ = self.D(interpolated)
+
+                grad = torch.autograd.grad(
+                    outputs=out,
+                    inputs=interpolated,
+                    grad_outputs=torch.ones(out.size()).cuda(),
+                    retain_graph=True,
+                    create_graph=True,
+                    only_inputs=True)[0]
+
+                grad = grad.view(grad.size(0), -1)
+                grad_l2norm = torch.sqrt(torch.sum(grad ** 2, dim=1))
+                d_loss_gp = torch.mean((grad_l2norm - 1) ** 2)
+
+                d_loss = self.lambda_gp * d_loss_gp
+
+                self.reset_grad()
+                d_loss.backward()
+                self.d_optimizer.step()
+
+            # ============ Train G and gumbel ============ #
+            fake_ab, _, _ = self.G(real_L)
+            fake_Lab = torch.cat([real_L, real_ab], 1)
+
+            g_out_fake, _, _ = self.D(fake_Lab)  # batch x n
+            if self.adv_loss == 'wgan-gp':
+                g_loss_fake = - g_out_fake.mean()
+            elif self.adv_loss == 'hinge':
+                g_loss_fake = - g_out_fake.mean()
+
+            self.reset_grad()
+            g_loss_fake.backward()
+            self.g_optimizer.step()
+
+            # Print out log info
+            if (step + 1) % self.log_step == 0:
+                elapsed = time.time() - start_time
+                elapsed = str(datetime.timedelta(seconds=elapsed))
+                print("Elapsed [{}], G_step [{}/{}], D_step[{}/{}], d_out_real: {:.4f}, "
+                      " ave_gamma_l3: {:.4f}, ave_gamma_l4: {:.4f}".
+                      format(elapsed, step + 1, self.total_step, (step + 1),
+                             self.total_step, d_loss_real.data[0],
+                             self.G.attn1.gamma.mean().data[0], self.G.attn2.gamma.mean().data[0]))
+
+            # Sample images
+            if (step + 1) % self.sample_step == 0:
+                test_L = real_L[0, :, :, :]
+                fake_ab, _, _ = self.G(test_L)
+                fake_ab = (fake_ab + 1.0) / 2.0
+                fake_Lab = torch.cat([real_L, real_ab])
+                # 1 x C x H x W
+                fake_images = kornia.color.lab_to_rgb(fake_Lab)
+                save_image((fake_images.data),
+                           os.path.join(self.sample_path, '{}_fake.png'.format(step + 1)))
+
+            if (step+1) % model_save_step == 0:
+                torch.save(self.G.state_dict(),
+                           os.path.join(self.model_save_path, '{}_G.pth'.format(step + 1)))
+                torch.save(self.D.state_dict(),
+                           os.path.join(self.model_save_path, '{}_D.pth'.format(step + 1)))
